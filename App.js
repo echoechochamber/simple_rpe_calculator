ff92911860c206b5035b2909d69b816eccbcca0f
A.K. Williams <akwillia2@gmail.com>
2019-09-21 17:28:44 -0400

    [refactor] app.js as component to facilitate custom font import

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   StyleSheet,
   Switch,
@@ -13,60 +13,7 @@ import RpeDisplay from "./src/rpeDisplay";
 import { calculateE1RM } from "./src/lib";
 import * as Font from "expo-font";
 
-export default function App() {
-  const [refRpe, setRefRpe] = useState(6);
-  const [refReps, setRefReps] = useState(1);
-  const [targetReps, setTargetReps] = useState(1);
-  const [weight, setWeight] = useState("0");
-  const [hasSmallWeights, toggleSmallWeights] = useState(false);
-  const rpeValues = ["6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10"];
-
-  return (
-    <View style={styles.container}>
-      <View>
-        <FlatList
-          data={rpeValues}
-          keyExtractor={rpeVal => rpeVal}
-          renderItem={({ item }) => (
-            <RpeDisplay
-              e1RM={calculateE1RM(weight, refReps, refRpe)}
-              repCount={targetReps}
-              rpe={item}
-              hasSmallWeights={hasSmallWeights}
-            />
-          )}
-        />
-      </View>
-      {/* bottom container */}
-      <View style={styles.bottomContainer}>
-        <View style={styles.referenceContainers}>
-          <Text style={styles.sectionTitle}>Target Numbers</Text>
-          <TextInput
-            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-            onChangeText={text => setWeight(text)}
-            value={weight}
-            keyboardType={"numeric"}
-            clearTextOnFocus={true}
-          />
-          <RpePicker RPE={refRpe} onChangeRpe={newRpe => setRefRpe(newRpe)} />
-          <RepsPicker reps={refReps} onChangeReps={reps => setRefReps(reps)} />
-        </View>
-
-        <View style={styles.referenceContainers}>
-          <Text style={styles.sectionTitle}>Target Numbers</Text>
-          <RepsPicker
-            reps={targetReps}
-            onChangeReps={reps => setTargetReps(reps)}
-          />
-          <Switch
-            onValueChange={value => toggleSmallWeights(value)}
-            value={hasSmallWeights}
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
+const rpeValues = ["6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10"];
 
 const styles = StyleSheet.create({
   container: {
@@ -97,3 +44,80 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refRpe: 6,
+      refReps: 1,
+      targetReps: 1,
+      weight: "0",
+      hasSmallWeights: false,
+    };
+  }
+
+  ORM() {
+    return calculateE1RM(
+      this.state.weight,
+      this.state.refReps,
+      this.state.refRpe
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View style={styles.container}>
+        <View>
+          <FlatList
+            data={rpeValues}
+            extraData={this.state}
+            keyExtractor={rpeVal => rpeVal}
+            renderItem={({ item }) => (
+              <RpeDisplay
+                e1RM={this.ORM()}
+                repCount={this.state.targetReps}
+                rpe={parseFloat(item)}
+                hasSmallWeights={this.state.hasSmallWeights}
+              />
+            )}
+          />
+        </View>
+        {/* bottom container */}
+        <View style={styles.bottomContainer}>
+          <View style={styles.referenceContainers}>
+            <Text style={styles.sectionTitle}>Target Numbers</Text>
+            <TextInput
+              style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+              onChangeText={text => this.setState({ weight: text })}
+              value={this.state.weight}
+              keyboardType={"numeric"}
+              clearTextOnFocus={true}
+            />
+            <RpePicker
+              RPE={this.state.refRpe}
+              onChangeRpe={newRpe => this.setState({ refRpe: newRpe })}
+            />
+            <RepsPicker
+              reps={this.state.refReps}
+              onChangeReps={reps => this.setState({ refReps: reps })}
+            />
+          </View>
+
+          <View style={styles.referenceContainers}>
+            <Text style={styles.sectionTitle}>Target Numbers</Text>
+            <RepsPicker
+              reps={this.state.targetReps}
+              onChangeReps={reps => this.setState({ targetReps: reps })}
+            />
+            <Switch
+              onValueChange={value => this.setState({ hasSmallWeights: value })}
+              value={this.state.hasSmallWeights}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
