c9a9595fc872438bdec599031012f8dfb983254f
A.K. Williams <akwillia2@gmail.com>
2019-09-22 00:34:15 -0400

    [add] label to repsPicker

--- src/repsPicker.js
+++ src/repsPicker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Picker, StyleSheet } from "react-native";
+import { Picker, StyleSheet, View, Text } from "react-native";
 import PropTypes from "prop-types";
 
 const RepsPicker = props => {
@@ -8,23 +8,26 @@ const RepsPicker = props => {
   };
 
   return (
-    <Picker
-      selectedValue={props.reps}
-      style={styles.picker}
-      onValueChange={(itemValue, itemIndex) => changeReps(itemValue)}>
-      <Picker.Item label="1" value="1" />
-      <Picker.Item label="2" value="2" />
-      <Picker.Item label="3" value="3" />
-      <Picker.Item label="4" value="4" />
-      <Picker.Item label="5" value="5" />
-      <Picker.Item label="6" value="6" />
-      <Picker.Item label="7" value="7" />
-      <Picker.Item label="8" value="8" />
-      <Picker.Item label="9" value="9" />
-      <Picker.Item label="10" value="10" />
-      <Picker.Item label="11" value="11" />
-      <Picker.Item label="12" value="12" />
-    </Picker>
+    <View>
+      <Text style={styles.label}>Reps</Text>
+      <Picker
+        selectedValue={props.reps}
+        style={styles.picker}
+        onValueChange={(itemValue, itemIndex) => changeReps(itemValue)}>
+        <Picker.Item label="1" value="1" />
+        <Picker.Item label="2" value="2" />
+        <Picker.Item label="3" value="3" />
+        <Picker.Item label="4" value="4" />
+        <Picker.Item label="5" value="5" />
+        <Picker.Item label="6" value="6" />
+        <Picker.Item label="7" value="7" />
+        <Picker.Item label="8" value="8" />
+        <Picker.Item label="9" value="9" />
+        <Picker.Item label="10" value="10" />
+        <Picker.Item label="11" value="11" />
+        <Picker.Item label="12" value="12" />
+      </Picker>
+    </View>
   );
 };
 
@@ -38,6 +41,10 @@ const styles = StyleSheet.create({
     margin: "4px",
     height: "30px",
     width: "120px",
+  },
+  label: {
+    paddingLeft: "4px",
+    fontFamily: "cabin-semibold",
   },
 });
 
